9ccd591ca93205de587eebf2f7614098baa611b5
Hemin Joshi <joshihemin@gmail.com>
2020-04-20 22:25:57 -0400

    Navigation Bar Bug Fixes

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Hemin Joshi on 4/20/20.
+//  Copyright © 2020 Hemin Joshi. All rights reserved.
+//
+
+import Foundation
+struct Follower : Codable {
+    var login : String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Hemin Joshi on 4/20/20.
+//  Copyright © 2020 Hemin Joshi. All rights reserved.
+//
+
+import Foundation
+struct User : Codable {
+    var login : String
+    var avatarUrl : String
+    var name : String?
+    var location : String?
+    var bio : String?
+    var publicRepos : Int
+    var publicGists : Int
+    var htmlUrl: String
+    var following : Int
+    var followers : Int
+    var createdAt : String
+}
