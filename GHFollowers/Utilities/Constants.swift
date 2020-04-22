7491436751710287af06215f91f4bbb7735939d0
Hemin Joshi <joshihemin@gmail.com>
2020-04-22 18:14:53 -0400

    Added UserInfoHeaderVC - Child View Controller

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Hemin Joshi on 4/22/20.
+//  Copyright © 2020 Hemin Joshi. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
